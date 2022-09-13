622c521ee9bbe799dbf74bb6cc72aef5f54a4e79
gerson <narrow_06@hotmail.com>
2022-09-13 15:23:26 -0300

    mi quinto comit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar"
 import ItemListContainer from "./Containers/ItemListContainer/ItemListContainer";
+import ItemCount from "./ItemCount/index"
 
 
 
-const App = () =>{
+
+const App = () => {
  
    
   const mensaje = "Hola!!! Bienvenidos a Italian Vaqueria"
+  
   
 
 
   return (
       <>
         <Navbar/>
-        
         <ItemListContainer greeting= {mensaje}/>
+        {/* <ItemCount initial={1} stock={6} onAdd={onAdd} /> */}
+        <ItemCount initial={1} stock={10} onAdd={() => {}}/>
       </>
    
     ) 
--- src/Containers/ItemListContainer/ItemListContainer.js
+++ src/Containers/ItemListContainer/ItemListContainer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const ItemListContainer = ({greeting}) => {
   return (
-    
-    <h1>{greeting}</h1>
+    <>
+       <h1>{greeting}</h1>
+    </>
   )
 }
 
