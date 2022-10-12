d8f92b25fbf538a374e3334a1a4580d4f5d39313
gerson <narrow_06@hotmail.com>
2022-10-12 00:53:26 -0300

    ultimo repositorio 11/10

--- src/Containers/ItemDetailContainer/index.jsx
+++ src/Containers/ItemDetailContainer/index.jsx
@@ -4,7 +4,8 @@ import CircularProgress from '@mui/material/CircularProgress';
 import ItemDetail from '../ItemDetail';
 import { products } from '../../assets/productos';
 import { useParams } from 'react-router-dom';
-
+import { database } from '../../firebase/firebase';
+import { getDoc, collection, doc } from 'firebase/firestore';
 
 const ItemDetailContainer = () => {
 
@@ -15,8 +16,29 @@ const ItemDetailContainer = () => {
 
 
   useEffect(() => {
-    customFetch(products, 0, parseInt(id)).then((res) => setProducto(res))
+    const productCollection = collection(database, "products");
+    const refDoc = doc(productCollection, id)
+    getDoc(refDoc)
+      .then((result) => {
+        setProducto(
+        {
+          id: result.id,
+          ...result.data()
+        }
+        )
+      })
+
+
+
+    // customFetch(products, 0, parseInt(id)).then((res) => setProducto(res))
   }, [id])
+
+
+
+
+
+
+
 
   console.log(producto)
 
